1e266f0318d001c0f2011084003f889fe4543da6
Matias Arias <matiasarias384@gmail.com>
2022-09-04 23:01:46 -0300

    MVP: ABMC Marca

---
+++ src/concesionaria/ConexionMySQL.java
@@ -0,0 +1,19 @@
+package concesionaria;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+public class ConexionMySQL {
+    Connection cn;
+    
+    public Connection conectar(){
+    try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn=(Connection) DriverManager.getConnection("jdbc:mysql://localhost/concesionaria","root","");
+            System.out.println("CONECTADO");
+    }catch (Exception e){
+        System.out.println("ERROR:"+e);
+    }
+    return cn;
+    }
+}
