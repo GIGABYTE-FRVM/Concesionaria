62c1de32bd924633b55878b71433fc1427e92a39
Matias Arias <matiasarias384@gmail.com>
2022-09-14 15:27:32 -0300

    Upgrade to Hibernate

---
+++ src/main/java/net/javaguides/hibernate/dao/PaisDao.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.javaguides.hibernate.dao;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import net.javaguides.hibernate.model.Pais;
+import net.javaguides.hibernate.util.HibernateUtil;
+
+/**
+ *
+ * @author matya
+ */
+public class PaisDao implements iPaisDao {
+
+    //save Marca
+    //get All Marca
+    //get Marca By Id
+    //Update Marca
+    //Delete Marca
+    @Override
+    public void savePais(Pais pais) {
+        Transaction transaction = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            
+            session.persist(pais);
+            
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }@Override
+    public void updatePais(Pais pais) {
+        Transaction transaction = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            session.saveOrUpdate(pais);
+            
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+    @Override
+    public Pais getPaisById(int id) {
+        Transaction transaction = null;
+        Pais pais = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            pais = session.get(Pais.class,id);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return pais;
+    }
+    @Override
+    public List<Pais> getAllPais() {
+        Transaction transaction = null;
+        List<Pais> paises = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            paises = session.createQuery("from Pais").list();
+
+            transaction.commit();
+                
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return paises;
+    }
+    @Override
+    public void deletePais(int id) {
+        Transaction transaction = null;
+        Pais pais = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            pais = session.get(Pais.class,id);
+            session.delete(pais);
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+}
---
+++ src/main/java/net/javaguides/hibernate/model/Marca.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.javaguides.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="marca")
+
+public class Marca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "codigo")
+    private String codigo;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+
+    public Marca() {
+    }
+
+    public Marca(String codigo, String nombre, String descripcion) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Marca [id=" + id + ", codigo=" + codigo + ", nombre=" + nombre + ", descripcion=" + descripcion + "]";
+    }
+}
